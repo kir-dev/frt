964b740a155134ba9fd90dda3592436609fa873d
Vaska Barnabás <vaska.barnabas@gmail.com>
2025-07-22 16:33:13 +0200

    Refactored pageContent.tsx and page.tsx

--- src/app/(app)/tamogass-minket/page.tsx
+++ src/app/(app)/tamogass-minket/page.tsx
@@ -1,4 +1,4 @@
-import {sections} from "./pageContent"
+import {pageItems} from "./pageContent"
 import Image from "next/image"
 import Link from "next/link";
 
@@ -12,7 +12,7 @@ export default async function DonatePage(props: { searchParams?: Promise<Record<
     return (
         <main className="min-h-screen bg-black text-white">
             <div className="container mx-auto px-4 py-12 max-w-5xl">
-                <h1 className="text-3xl font-bold mb-8">{lang === "en" ? sections.title_eng : sections.title}</h1>
+                <h1 className="text-3xl font-bold mb-8">{lang === "en" ? pageItems.title_eng : pageItems.title}</h1>
                 <div className="mb-12">
                     <a className="mb-4 max-w-[250px] text-center inline-block bg-red-600 hover:bg-red-700 !text-white font-bold py-2 px-6 rounded-lg transition-colors text-base w-full"
                        href='mailto:sponsoring@fun-ke.com'
@@ -20,38 +20,31 @@ export default async function DonatePage(props: { searchParams?: Promise<Record<
                         {lang === "en" ? "I want to support the team" : "Támogaton a csapatot"}
                     </a>
                 </div>
-                <h2 className="text-xl font-bold mb-6">{lang === "en" ? sections.subtitle1_eng : sections.subtitle1}</h2>
-                <p className="text-xl mb-8">{lang === "en" ? sections.subsections_eng[0] : sections.subsections[0]}{" "}
-                    <Link href="/rolunk">
-                        <span
-                            className="text-lg text-frtRed hover:underline">{lang === "en" ? sections.moreButtonText_eng : sections.moreButtonText}</span>
-                    </Link>
-                </p>
-                <div className="self-center mb-6">
-                    <Image src={"/csapatkep.jpg"} alt={"Team picture"} height={300} width={700}
-                           className="rounded-xl mx-auto"/>
-                </div>
-                <div className="flex flex-row justify-center mb-12">
-                    <a className="mb-4 max-w-[250px] text-center inline-block bg-red-600 hover:bg-red-700 !text-white font-bold py-2 px-6 rounded-lg transition-colors text-base w-full"
-                       href='mailto:sponsoring@fun-ke.com'
-                    >
-                        {lang === "en" ? "I want to support the team" : "Támogatom a csapatot"}
-                    </a>
-                </div>
-                <h2 className="text-xl font-bold mb-6">{lang === "en" ? sections.subtitle2_eng : sections.subtitle2}</h2>
-                <p className="text-xl mb-8">{lang === "en" ? sections.subsections_eng[1] : sections.subsections[1]}
-                </p>
-                <div className="self-center mb-6">
-                    <Image src={"/FRT_FormulaStudent1.jpg"} alt={"Team picture"} height={300} width={700}
-                           className="rounded-xl mx-auto"/>
-                </div>
-                <div className="flex flex-row justify-center mb-12">
-                    <a className="mb-4 max-w-[250px] text-center inline-block bg-red-600 hover:bg-red-700 !text-white font-bold py-2 px-6 rounded-lg transition-colors text-base w-full"
-                       href='mailto:sponsoring@fun-ke.com'
-                    >
-                        {lang === "en" ? "I want to support the team" : "Támogatom a csapatot"}
-                    </a>
-                </div>
+                {pageItems.sections.map((section, index) => (
+                    <div key={index}>
+                        <h2 className="text-xl font-bold mb-6">{lang === "en" ? section.subtitle_eng : section.subtitle}</h2>
+                        <p className="text-xl mb-8">{lang === "en" ? section.subsection_eng : section.subsection}{" "}
+                            { index === 0 &&
+                               <Link href="/rolunk">
+                                <span
+                                   className="text-lg text-frtRed hover:underline">{lang === "en" ? pageItems.moreButtonText_eng : pageItems.moreButtonText}</span>
+                               </Link>
+                            }
+                        </p>
+                        <div className="self-center mb-6">
+                            <Image src={"/csapatkep.jpg"} alt={"Team picture"} height={300} width={700}
+                                   className="rounded-xl mx-auto"/>
+                        </div>
+                        <div className="flex flex-row justify-center mb-12">
+                            <a className="mb-4 max-w-[250px] text-center inline-block bg-red-600 hover:bg-red-700 !text-white font-bold py-2 px-6 rounded-lg transition-colors text-base w-full"
+                               href='mailto:sponsoring@fun-ke.com'
+                            >
+                                {lang === "en" ? "I want to support the team" : "Támogatom a csapatot"}
+                            </a>
+                        </div>
+
+                    </div>
+                ))}
             </div>
             {/*<p className="text-xl">{lang === "en" ? sections.subtitle_eng : sections.subtitle}</p>*/}
         </main>
--- src/app/(app)/tamogass-minket/pageContent.tsx
+++ src/app/(app)/tamogass-minket/pageContent.tsx
@@ -1,17 +1,35 @@
-export const sections = {
+type Sections = {
+    subtitle: string;
+    subtitle_eng: string;
+    subsection: string;
+    subsection_eng: string;
+}
+
+type PageItem = {
+    title: string;
+    title_eng: string;
+    moreButtonText: string;
+    moreButtonText_eng: string;
+    sections: Sections[];
+}
+
+export const pageItems : PageItem = {
     title: "Támogasd Csapatunkat",
     title_eng: "Support our Team",
-    subtitle1: "Kik vagyunk mi?",
-    subtitle1_eng: "Who we are?",
-    subtitle2: "Miért támogass minket?",
-    subtitle2_eng: "Why support us?",
-    subsections: [
-        "Mi vagyunk a BME Formula Racing Team – egy lelkes, egyetemistákból álló csapat, akik nem csak álmodoznak versenyautókról, hanem saját kezűleg meg is építik azt. Célunk, hogy minden évben egy saját tervezésű és építésű versenyautót alkossunk, amivel nemzetközi versenyeken vehetünk részt.",
-        "A projektünk nem csupán egy autóról szól – ez egy komplex mérnöki kihívás, valódi csapatmunka, kreativitás és hatalmas elhivatottság. Minden egyes támogatás közelebb visz minket ahhoz, hogy fejlettebb technológiákkal dolgozhassunk, professzionálisabb autót építhessünk, és méltó módon képviselhessük egyetemünket és hazánkat a világversenyeken."
-    ],
-    subsections_eng: [
-        "We are the BME Formula Racing Team – a dedicated group of university students who don’t just dream about race cars, but build it with their own hands. Our goal is to design and manufacture a new race car each year, allowing us to compete in international competitions.",
-        "Our project is about more than just building a car – it’s a complex engineering challenge that requires true teamwork, creativity, and deep commitment. Every bit of support brings us closer to working with more advanced technologies, building a more professional vehicle, and proudly representing our university and our country on the global stage."
+    sections: [
+        {
+            subtitle: "Kik vagyunk mi?",
+            subtitle_eng: "Who we are?",
+            subsection: "Mi vagyunk a BME Formula Racing Team – egy lelkes, egyetemistákból álló csapat, akik nem csak álmodoznak versenyautókról, hanem saját kezűleg meg is építik azt. Célunk, hogy minden évben egy saját tervezésű és építésű versenyautót alkossunk, amivel nemzetközi versenyeken vehetünk részt.",
+            subsection_eng: "We are the BME Formula Racing Team – a dedicated group of university students who don’t just dream about race cars, but build it with their own hands. Our goal is to design and manufacture a new race car each year, allowing us to compete in international competitions.",
+        },
+        {
+            subtitle: "Miért támogass minket?",
+            subsection_eng: "Why support us?",
+            subsection: "A projektünk nem csupán egy autóról szól – ez egy komplex mérnöki kihívás, valódi csapatmunka, kreativitás és hatalmas elhivatottság. Minden egyes támogatás közelebb visz minket ahhoz, hogy fejlettebb technológiákkal dolgozhassunk, professzionálisabb autót építhessünk, és méltó módon képviselhessük egyetemünket és hazánkat a világversenyeken.",
+            subtitle_eng: "Our project is about more than just building a car – it’s a complex engineering challenge that requires true teamwork, creativity, and deep commitment. Every bit of support brings us closer to working with more advanced technologies, building a more professional vehicle, and proudly representing our university and our country on the global stage.",
+        }
+
     ],
     moreButtonText: "Bővebben a csapatról...",
     moreButtonText_eng: "Learn more about the team..."
