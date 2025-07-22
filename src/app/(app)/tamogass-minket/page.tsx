2c231e99c03a292d1f1ee9143b2094593b8a16da
Vaska Barnabás <vaska.barnabas@gmail.com>
2025-07-22 16:10:54 +0200

    Changed the design to make it cleaner

--- src/app/(app)/tamogass-minket/page.tsx
+++ src/app/(app)/tamogass-minket/page.tsx
@@ -27,11 +27,22 @@ export default async function DonatePage(props: { searchParams?: Promise<Record<
                             className="text-lg text-frtRed hover:underline">{lang === "en" ? sections.moreButtonText_eng : sections.moreButtonText}</span>
                     </Link>
                 </p>
+                <div className="self-center mb-6">
+                    <Image src={"/csapatkep.jpg"} alt={"Team picture"} height={300} width={700}
+                           className="rounded-xl mx-auto"/>
+                </div>
+                <div className="flex flex-row justify-center mb-12">
+                    <a className="mb-4 max-w-[250px] text-center inline-block bg-red-600 hover:bg-red-700 !text-white font-bold py-2 px-6 rounded-lg transition-colors text-base w-full"
+                       href='mailto:sponsoring@fun-ke.com'
+                    >
+                        {lang === "en" ? "I want to support the team" : "Támogatom a csapatot"}
+                    </a>
+                </div>
                 <h2 className="text-xl font-bold mb-6">{lang === "en" ? sections.subtitle2_eng : sections.subtitle2}</h2>
                 <p className="text-xl mb-8">{lang === "en" ? sections.subsections_eng[1] : sections.subsections[1]}
                 </p>
                 <div className="self-center mb-6">
-                    <Image src={"/csapatkep.jpg"} alt={"Team picture"} height={300} width={700}
+                    <Image src={"/FRT_FormulaStudent1.jpg"} alt={"Team picture"} height={300} width={700}
                            className="rounded-xl mx-auto"/>
                 </div>
                 <div className="flex flex-row justify-center mb-12">
