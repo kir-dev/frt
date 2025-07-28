8bb3daef249c9189dae5a9e5642466abaf996f31
Vaska Barnabás <vaska.barnabas@gmail.com>
2025-07-28 15:44:40 +0200

    Created a card for the different galleries

---
+++ src/components/galleryCard.tsx
@@ -0,0 +1,40 @@
+import {Media} from "@/payload-types";
+import Image from "next/image";
+import {Gallery} from "@/payload-types"
+
+interface Props {
+    gallery: Gallery;
+}
+function dateFormatter(date: string):string {
+    const realDate = new Date(date);
+    const month = String(realDate.getMonth() + 1).padStart(2, '0');
+    const day = String(realDate.getDate()).padStart(2, '0');
+    return `${month}.${day}.`;
+
+}
+
+export default function GalleryCard({gallery}: Props) {
+    const monthAndDay = dateFormatter(gallery.date);
+    const images: Media[] = Array.isArray(gallery?.images)
+        ? gallery.images
+            .map((item) => item.image)
+            .filter((img): img is Media => typeof img === 'object' && img !== null)
+        : [];
+
+    const mainImage: Media = images[0];
+
+
+    return (
+        <div className="bg-frtcardBG rounded-lg overflow-hidden">
+            <div>
+                <p className="bg-black text-white absolute z-10">{monthAndDay}</p>
+              <Image
+                    src={mainImage?.url || "/placeholder.svg"}
+                    alt={mainImage?.alt || gallery.title}
+              height={150}
+              width={200}/>
+            </div>
+            <h3 className="text-white">{gallery.title}</h3>
+        </div>
+    )
+}
