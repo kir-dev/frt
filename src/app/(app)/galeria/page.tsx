6a1c6d1a76e36c7ec7f311d5668913f91306775e
Vaska Barnabás <vaska.barnabas@gmail.com>
2025-07-28 20:59:20 +0200

    Gallery is now grouped by month

--- src/app/(app)/galeria/page.tsx
+++ src/app/(app)/galeria/page.tsx
@@ -1,51 +1,121 @@
 import type React from "react";
-import {getGallery} from "@/lib/payload-cms";
+import { getGallery } from "@/lib/payload-cms";
 import GalleryCard from "@/components/galleryCard";
+import { Gallery } from "@/payload-types";
 
-export default async function GalleryPage(props: { searchParams?: Promise<Record<string, string>> }) {
-    let lang = 'hu';
-    if (props?.searchParams) {
-        const sp = await props.searchParams;
-        lang = sp && 'lang' in sp && sp.lang === 'en' ? 'en' : 'hu';
+function groupByDate(galleries: Gallery[]) {
+  return galleries.reduce((map, item) => {
+    const dateObj = new Date(item.date);
+    const year = dateObj.getFullYear();
+    const month = String(dateObj.getMonth() + 1).padStart(2, "0"); // months are 0-indexed
+    const key = `${year}-${month}`;
+    if (!map.has(key)) {
+      map.set(key, []);
     }
-
-    const translations = {
-        title: lang === "en" ? "Gallery" : "Képek",
-        noPictures: lang === "en" ? "There are currently no available pictures.": "Jelenleg nincseenek elérhető képek",
-    }
-
-    const galleries  = await getGallery();
-
-    if (galleries.length === 0) {
-        return (
-            <main className="min-h-screen bg-black text-white">
-                <div className="container mx-auto px-4 py-12 max-w-5xl">
-                    <h1 className="text-3xl font-bold text-center mb-12">{translations.title}</h1>
-                    <p className="text-center text-xl">{translations.noPictures}</p>
-                </div>
-            </main>
-        );
-    }
-
-    return (
-        <main className="min-h-screen bg-black text-white">
-            <div className="bg-black container mx-auto py-12 max-w-5xl">
-                <h1 className="text-3xl font-bold mb-12">{translations.title}</h1>
-                <div className="bg-black flex flex-col container mx-auto py-12 pb-0 max-w-5xl">
-                    <div className="h-0.5 !bg-frtRed w-full mx-auto mb-2"></div>
-                    <h2 className="text-xl font-bold">Januar</h2>
-                </div>
-                {/*Ide jonnek a kepek*/}
-                <div className="grid grid-cols-3 gap-4">
-                    {galleries.map((gallery, index) => {
-                        return (
-                            <GalleryCard gallery={gallery} key={index}/>
-                        )
-                    })}
-                </div>
-            </div>
-
-
-        </main>
-    )
+    map.get(key).push(item);
+    return map;
+  }, new Map());
 }
+
+export default async function GalleryPage(props: {
+  searchParams?: Promise<Record<string, string>>;
+}) {
+  let lang = "hu";
+  if (props?.searchParams) {
+    const sp = await props.searchParams;
+    lang = sp && "lang" in sp && sp.lang === "en" ? "en" : "hu";
+  }
+
+  const translations = {
+    title: lang === "en" ? "Gallery" : "Képek",
+    noPictures:
+      lang === "en"
+        ? "There are currently no available pictures."
+        : "Jelenleg nincseenek elérhető képek",
+  };
+
+  const galleries = await getGallery();
+
+  const groupedGalleryMap = groupByDate(galleries);
+
+  if (galleries.length === 0) {
+    return (
+      <main className="min-h-screen bg-black text-white">
+        <div className="container mx-auto px-4 py-12 max-w-5xl">
+          <h1 className="text-3xl font-bold text-center mb-12">
+            {translations.title}
+          </h1>
+          <p className="text-center text-xl">{translations.noPictures}</p>
+        </div>
+      </main>
+    );
+  }
+
+  return (
+    <main className="min-h-screen bg-black text-white">
+      <div className="bg-black container mx-auto py-12 max-w-5xl">
+        <h1 className="text-4xl font-bold mb-12">{translations.title}</h1>
+
+        {[...groupedGalleryMap.keys()].map((dateKey, index) => {
+          const galleries: Gallery[] = groupedGalleryMap.get(dateKey);
+          const date = new Date(dateKey);
+
+          if (galleries.length === 0) {
+            return null;
+          }
+
+          return (
+            <div
+              className="bg-black flex flex-col container mx-auto py-12 pb-0 max-w-5xl"
+              key={index}
+            >
+              <div className="h-0.5 !bg-frtRed w-full mx-auto mb-2"></div>
+              <h2 className="text-2xl font-bold">
+                {lang === "en"
+                  ? monthsEnglish[date.getMonth()]
+                  : monthsHungarian[date.getMonth()]}
+              </h2>
+              <div className="grid grid-cols-3 gap-6 mt-8">
+                {galleries.map((gallery, index) => {
+                  return <GalleryCard gallery={gallery} key={index} />;
+                })}
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </main>
+  );
+}
+
+//<GalleryCard gallery={gallery} key={index}/>;
+
+const monthsEnglish: string[] = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+
+const monthsHungarian: string[] = [
+  "Január",
+  "Február",
+  "Március",
+  "Április",
+  "Május",
+  "Június",
+  "Július",
+  "Augusztus",
+  "Szeptember",
+  "Október",
+  "November",
+  "December",
+];
--- src/lib/payload-cms.ts
+++ src/lib/payload-cms.ts
@@ -1,112 +1,119 @@
-import {getPayload} from "payload";
+import { getPayload } from "payload";
 import config from "@payload-config";
-import {Group, Member, Car, Recruitment, Sponsor, Gallery} from "@/payload-types";
+import {
+  Group,
+  Member,
+  Car,
+  Recruitment,
+  Sponsor,
+  Gallery,
+} from "@/payload-types";
 export async function getSponsors(): Promise<Sponsor[]> {
-    const payload = await getPayload({ config })
-    const sponsors = await payload.find({
-        collection: "sponsors",
-        limit: 1000,
-    })
+  const payload = await getPayload({ config });
+  const sponsors = await payload.find({
+    collection: "sponsors",
+    limit: 1000,
+  });
 
-    return sponsors.docs as Sponsor[]
+  return sponsors.docs as Sponsor[];
 }
 
-export async function getRecruitmentData() : Promise<Recruitment[]> {
-    const payload = await getPayload({ config })
-    const recruitmentData = await payload.find({
-        collection: "recruitment",
-        limit: 1000,
-    })
-    return recruitmentData.docs
+export async function getRecruitmentData(): Promise<Recruitment[]> {
+  const payload = await getPayload({ config });
+  const recruitmentData = await payload.find({
+    collection: "recruitment",
+    limit: 1000,
+  });
+  return recruitmentData.docs;
 }
 
 export async function getCars(): Promise<Car[]> {
-    const payload = await getPayload({config})
-    const cars = await payload.find({
-        collection: "cars",
-        sort: "-year",
-        limit: 1000,
-    })
+  const payload = await getPayload({ config });
+  const cars = await payload.find({
+    collection: "cars",
+    sort: "-year",
+    limit: 1000,
+  });
 
-    return cars.docs as Car[]
+  return cars.docs as Car[];
 }
 
-export async function getMembers() : Promise<Member[]> {
-    const payload = await getPayload({ config })
-    const members = await payload.find({
-        collection: "members",
-        limit: 1000,
-        sort: "order",
-    })
+export async function getMembers(): Promise<Member[]> {
+  const payload = await getPayload({ config });
+  const members = await payload.find({
+    collection: "members",
+    limit: 1000,
+    sort: "order",
+  });
 
-    return members.docs as Member[]
+  return members.docs as Member[];
 }
 
-export async function getGroups() : Promise<Group[]> {
-    const payload = await getPayload({ config })
-    const groups = await payload.find({
-        collection: "groups",
-        limit: 1000,
-        sort: "order",
-    })
+export async function getGroups(): Promise<Group[]> {
+  const payload = await getPayload({ config });
+  const groups = await payload.find({
+    collection: "groups",
+    limit: 1000,
+    sort: "order",
+  });
 
-    return groups.docs as Group[]
+  return groups.docs as Group[];
 }
 
 export async function getArticles() {
-    const payload = await getPayload({ config })
-    const articles = await payload.find({
-        collection: "articles",
-        limit: 1000,
-        sort: "-published_date",
-    })
+  const payload = await getPayload({ config });
+  const articles = await payload.find({
+    collection: "articles",
+    limit: 1000,
+    sort: "-published_date",
+  });
 
-    return articles.docs
+  return articles.docs;
 }
 
 export async function getArticleBySlug(slug: string) {
-    const payload = await getPayload({ config })
-    const article = await payload.find({
-        collection: "articles",
-        where: {
-            slug: {
-                equals: slug,
-            },
-        },
-        limit: 1,
-    })
+  const payload = await getPayload({ config });
+  const article = await payload.find({
+    collection: "articles",
+    where: {
+      slug: {
+        equals: slug,
+      },
+    },
+    limit: 1,
+  });
 
-    return article.docs.length > 0 ? article.docs[0] : null
+  return article.docs.length > 0 ? article.docs[0] : null;
 }
 
 export async function getPublications() {
-    const payload = await getPayload({ config })
-    const publications = await payload.find({
-        collection: "publications",
-        limit: 1000,
-        sort: "-createdAt",
-    })
+  const payload = await getPayload({ config });
+  const publications = await payload.find({
+    collection: "publications",
+    limit: 1000,
+    sort: "-createdAt",
+  });
 
-    return publications.docs
+  return publications.docs;
 }
 
 export async function getAssociation() {
-    const payload = await getPayload({ config })
-    const association = await payload.find({
-        collection: "association",
-        limit: 1,
-    })
+  const payload = await getPayload({ config });
+  const association = await payload.find({
+    collection: "association",
+    limit: 1,
+  });
 
-    return association.docs.length > 0 ? association.docs[0] : null
+  return association.docs.length > 0 ? association.docs[0] : null;
 }
 
-export async function getGallery(): Promise<Gallery[]>{
-    const payload = await getPayload({ config })
-    const gallery = await payload.find({
-        collection: "gallery",
-        limit: 1000,
-    })
+export async function getGallery(): Promise<Gallery[]> {
+  const payload = await getPayload({ config });
+  const gallery = await payload.find({
+    collection: "gallery",
+    sort: "date",
+    limit: 1000,
+  });
 
-    return gallery.docs
+  return gallery.docs;
 }
-
