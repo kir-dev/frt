8913b52ab18c828854dbfa9b54e80826e099ae0b
Vaska Barnabás <vaska.barnabas@gmail.com>
2025-07-28 16:01:58 +0200

    Fixed gallery card layout

--- src/components/galleryCard.tsx
+++ src/components/galleryCard.tsx
@@ -1,40 +1,45 @@
-import {Media} from "@/payload-types";
+import { Media } from "@/payload-types";
 import Image from "next/image";
-import {Gallery} from "@/payload-types"
+import { Gallery } from "@/payload-types";
 
 interface Props {
-    gallery: Gallery;
+  gallery: Gallery;
 }
-function dateFormatter(date: string):string {
-    const realDate = new Date(date);
-    const month = String(realDate.getMonth() + 1).padStart(2, '0');
-    const day = String(realDate.getDate()).padStart(2, '0');
-    return `${month}.${day}.`;
-
+function dateFormatter(date: string): string {
+  const realDate = new Date(date);
+  const month = String(realDate.getMonth() + 1).padStart(2, "0");
+  const day = String(realDate.getDate()).padStart(2, "0");
+  return `${month}.${day}.`;
 }
 
-export default function GalleryCard({gallery}: Props) {
-    const monthAndDay = dateFormatter(gallery.date);
-    const images: Media[] = Array.isArray(gallery?.images)
-        ? gallery.images
-            .map((item) => item.image)
-            .filter((img): img is Media => typeof img === 'object' && img !== null)
-        : [];
+export default function GalleryCard({ gallery }: Props) {
+  const monthAndDay = dateFormatter(gallery.date);
+  const images: Media[] = Array.isArray(gallery?.images)
+    ? gallery.images
+        .map((item) => item.image)
+        .filter((img): img is Media => typeof img === "object" && img !== null)
+    : [];
 
-    const mainImage: Media = images[0];
+  const mainImage: Media = images[0];
 
-
-    return (
-        <div className="bg-frtcardBG rounded-lg overflow-hidden">
-            <div>
-                <p className="bg-black text-white absolute z-10">{monthAndDay}</p>
-              <Image
-                    src={mainImage?.url || "/placeholder.svg"}
-                    alt={mainImage?.alt || gallery.title}
-              height={150}
-              width={200}/>
-            </div>
-            <h3 className="text-white">{gallery.title}</h3>
-        </div>
-    )
+  return (
+    <div className="bg-frtcardBG rounded-lg overflow-hidden">
+      <div className="relative w-full">
+        <p className="bg-frtcardBG text-white font-semibold absolute z-10 p-2">
+          {monthAndDay}
+        </p>
+        <Image
+          src={mainImage?.url || "/placeholder.svg"}
+          alt={mainImage?.alt || gallery.title}
+          width={0}
+          height={0}
+          sizes="100vw"
+          className="w-full h-auto"
+        />
+      </div>
+      <h3 className="text-white text-center text-xl font-bold py-3">
+        {gallery.title}
+      </h3>
+    </div>
+  );
 }
