6329668f74684971abb3d76f118583d972eaf134
Vaska Barnabás <vaska.barnabas@gmail.com>
2025-07-29 16:55:12 +0200

    Made combobox items scrollable

--- src/components/comboBox.tsx
+++ src/components/comboBox.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import ScrollArea from "@/components/scrollArea";
 
 interface ComboboxProps {
   listValues?: string[];
@@ -21,13 +22,13 @@ export default function Combobox({ listValues = [], setYear }: ComboboxProps) {
     <div>
       <div
         onClick={() => setIsOpen((prevState) => !prevState)}
-        className="cursor-pointer bg-frtcardBG w-[100px] py-1 rounded-lg hover:scale-105"
+        className="cursor-pointer bg-frtcardBG brightness-150 w-[100px] py-1 rounded-lg hover:scale-105"
       >
         <p className="text-center font-semibold">{value}</p>
       </div>
 
       {isOpen && (
-        <div className="absolute w-[100px] z-10 bg-frtcardBG brightness-200 rounded-lg overflow-hidden">
+        <ScrollArea className="absolute z-10 bg-frtcardBG brightness-100 rounded-lg overflow-hidden">
           {listValues.map((item, index) => (
             <div key={index} className="cursor-pointer">
               <div onClick={() => handleSelect(item)}>
@@ -37,8 +38,10 @@ export default function Combobox({ listValues = [], setYear }: ComboboxProps) {
               </div>
             </div>
           ))}
-        </div>
+        </ScrollArea>
       )}
     </div>
   );
 }
+
+// className="absolute w-[100px] z-10 bg-frtcardBG brightness-200 rounded-lg overflow-hidden"
---
+++ src/components/scrollArea.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { cn } from "@/lib/utils";
+
+interface ScrollAreaProps {
+  height?: string; // e.g. "h-64", "h-[300px]"
+  width?: string;
+  children: React.ReactNode;
+  className?: string;
+}
+
+export default function ScrollArea({
+  height = "h-25",
+  width = "w-25",
+  children,
+  className = "",
+}: ScrollAreaProps) {
+  return (
+    <div className={`overflow-y-auto ${height} ${width} pt-2 ${className}`}>
+      <div className="space-y-2">{children}</div>
+    </div>
+  );
+}
