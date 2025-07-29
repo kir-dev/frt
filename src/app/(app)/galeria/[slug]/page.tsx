3e2df7da85b71efb05faae57dbb75c45364a9613
Vaska Barnabás <vaska.barnabas@gmail.com>
2025-07-29 14:09:51 +0200

    Reused the image-gallery component to so users can see the images bigger if they click on it

--- src/app/(app)/galeria/[slug]/page.tsx
+++ src/app/(app)/galeria/[slug]/page.tsx
@@ -2,6 +2,8 @@ import { getGalleryBySlug } from "@/lib/payload-cms";
 import { formatDate } from "@/lib/utils";
 import { Media } from "@/payload-types";
 import Image from "next/image";
+import { notFound } from "next/navigation";
+import { ImageGallery } from "@/components/image-gallery";
 
 interface Props {
   params: Promise<{ slug: string }>;
@@ -22,14 +24,19 @@ export default async function Page({ params, searchParams }: Props) {
   const awaitedParams = await params;
   const gallery = await getGalleryBySlug(awaitedParams.slug);
   if (!gallery) {
-    return <p>Nincsenek kepek</p>;
+    notFound();
   }
 
-  //Transform gallery images
-  const images: Media[] = Array.isArray(gallery?.images)
-    ? gallery.images
-        .map((item) => item.image)
-        .filter((img): img is Media => typeof img === "object" && img !== null)
+  const galleryImages = gallery.images
+    ? gallery.images.map((item) => {
+        const image =
+          item.image && typeof item.image === "object" ? item.image : null;
+        return {
+          id: image?.id?.toString() || String(item.image),
+          url: image?.url || "/placeholder.svg",
+          alt: image?.alt || gallery.title,
+        };
+      })
     : [];
 
   return (
@@ -41,25 +48,22 @@ export default async function Page({ params, searchParams }: Props) {
         <h2 className="text-2xl font-semibold text-center mb-12">
           {formatDate(gallery.date)}
         </h2>
-        <div className="grid grid-cols-3 gap-6 mt-8">
-          {images.map((image, index) => {
-            return (
-              <div
-                key={index}
-                className="w-full aspect-rectangle overflow-hidden rounded-lg"
-              >
-                <Image
-                  src={image?.url || "/placeholder.svg"}
-                  alt={image?.alt || gallery.title}
-                  width={200}
-                  height={150}
-                  className="object-cover w-full h-full"
-                />
-              </div>
-            );
-          })}
-        </div>
+
+        <ImageGallery images={galleryImages} />
       </div>
     </main>
   );
 }
+
+/*<div
+    key={index}
+    className="w-full aspect-rectangle overflow-hidden rounded-lg"
+>
+  <Image
+      src={image?.url || "/placeholder.svg"}
+      alt={image?.alt || gallery.title}
+      width={200}
+      height={150}
+      className="object-cover w-full h-full"
+  />
+</div>*/
