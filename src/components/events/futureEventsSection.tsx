1f0ec1c004bfafcecf1239f5a07395051ff76c1f
Vaska Barnabás <vaska.barnabas@gmail.com>
2025-08-13 10:09:51 +0200

    Added a text to let the user know of there are not upcomming event

--- src/components/events/futureEventsSection.tsx
+++ src/components/events/futureEventsSection.tsx
@@ -12,16 +12,23 @@ export default function FutureEventsSection({
 }: futureEventSectionProps) {
   const translations = {
     title: lang === "en" ? "Upcomming Events" : "Aktuális Események",
+    noEventsMessage:
+      lang === "en"
+        ? "There are no upcomming events."
+        : "Jelenleg nincs aktuális esemény.",
   };
 
   return (
     <>
       <h1 className="text-4xl font-bold mb-12">{translations.title}</h1>
-      <div className="grid grid-cols-1 sm:grid-cols-2 gap-8">
-        {events.map((event) => (
-          <EventCard key={event.id} event={event} lang={lang} />
-        ))}
-      </div>
+      {events.length === 0 && <p>{translations.noEventsMessage}</p>}
+      {events.length > 0 && (
+        <div className="grid grid-cols-1 sm:grid-cols-2 gap-8">
+          {events.map((event) => (
+            <EventCard key={event.id} event={event} lang={lang} />
+          ))}
+        </div>
+      )}
     </>
   );
 }
