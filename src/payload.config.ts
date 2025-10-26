e79f892fb5897ded3bc52212b9f5270e4ddf82db
Horváth István <horvath.isti@edu.bme.hu>
2025-10-26 16:51:49 +0100

    Remove s3 support

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -20,7 +20,6 @@ import { Members } from "./collections/Members";
 import { Recruitment } from "./collections/Recruitment";
 import { Sponsors } from "@/collections/Sponsors";
 import Groups from "@/collections/Groups";
-import {s3Storage} from "@payloadcms/storage-s3";
 import Association from "@/collections/Association";
 import SupportUs from "@/collections/SupportUs";
 import Contact from "@/collections/Contact";
@@ -71,22 +70,6 @@ export default buildConfig({
   sharp,
   plugins: [
     payloadCloudPlugin(),
-    s3Storage({
-      collections: {
-          media: true
-      },
-      bucket: process.env.S3_BUCKET!,
-      config: {
-        credentials: {
-          accessKeyId: process.env.S3_ACCESS_KEY_ID!,
-          secretAccessKey: process.env.S3_SECRET_ACCESS_KEY!,
-        },
-        forcePathStyle: true,
-        region: process.env.S3_REGION,
-        endpoint: process.env.S3_ENDPOINT,
-        // ... Other S3 configuration
-      },
-    }),
     // storage-adapter-placeholder
   ],
 });
